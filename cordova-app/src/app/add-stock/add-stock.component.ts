846dd10f1096bfe31912f3d0beb06ed446dec84c
Atishay J <ajain@legalzoom.com>
2018-06-14 13:09:29 +0530

    Add Stock component created
    Add Stock component with browse option for image created

--- cordova-app/src/app/add-stock/add-stock.component.ts
+++ cordova-app/src/app/add-stock/add-stock.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-add-stock',
@@ -7,9 +7,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddStockComponent implements OnInit {
 
+  @ViewChild('browseImage') browseImage:ElementRef;
+  @ViewChild('visibleImageName') visibleImageName:ElementRef;
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  triggerBrowse() {
+    this.browseImage.nativeElement.click();    
+  }
+
+  updateImageName(){
+    let actualImagePath = this.browseImage.nativeElement.value;
+    let imageName = actualImagePath.replace(/.*[\/\\]/, '');
+
+    if((actualImagePath.indexOf('png') > -1) || (actualImagePath.indexOf('jpg') > -1) || (actualImagePath.indexOf('gif') > -1)){
+      this.visibleImageName.nativeElement.value = imageName;
+    } else {
+      this.browseImage.nativeElement.value = '';
+      console.log("You can upload an image file only");
+    }    
   }
 
 }
